951707ef8d0121d3655cf1794653294d4f818c4b
Duke Okeke <dukeokeke@gmail.com>
2022-05-27 20:50:47 -0600

    SmartNotes

---
+++ README.md
@@ -0,0 +1,22 @@
+# Overview
+This is a web application I developed as I learned the intricacies of Django. 
+I made a notes application where in users can join by creating an account and writing their own notes.
+
+
+[Software Demo Video](https://youtu.be/c-H5wiIALBA)
+
+# Web Pages
+There are several web pages here, but they are ancored together using the {% extends %} feature.
+There is a home page, login page, goodbye page amd so on.
+# Development Environment
+This software was developed in Visual Studio
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [LinkedIn Learning](https://www.linkedin.com/learning/django-essential-training/let-s-add-some-style?autoSkip=true&autoplay=true&contextUrn=urn%3Ali%3AlyndaLearningPath%3A5d546c44498e876bef6651ba&resume=false&u=2153100)
+
+# Future Work
+
+* I want to be able to create a more responsive and interactive website
+* I will try to develop a game 
---
+++ notes/views.py
@@ -0,0 +1,56 @@
+from http.client import HTTPResponse
+from django.http.response import HttpResponseRedirect
+from typing import List
+from django.shortcuts import render
+
+from django.http import Http404
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import NotesForm
+from .models import Notes
+# Create your views here.
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm 
+
+class NotesCreateView(LoginRequiredMixin, CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+    login_url = "/admin"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404('Note does not exist, you Dummy!')
+    return render(request, 'notes/notes_detail.html', {'note': note})
